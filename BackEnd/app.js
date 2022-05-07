3a159525628e67e4ca0d90f6a90662f9e404f9e1
shera-s <sheraofficials@gmail.com>
2022-05-07 17:15:56 +0530

    fix

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -23,9 +23,9 @@ app.use("/api/v1", cart);
 
 // if(process.env.NODE_ENV === 'production'){
     app.use(express.static(path.join(__dirname,'../frontend/build')))
-    // app.get('*',(req,res)=>{
-    //     res.sendFile(path.resolve(__dirname,'../frontend/build/index.html'))
-    // })
+    app.get('*',(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'../frontend/build/index.html'))
+    })
 // }
 
 // Requiring FileUpload and using
