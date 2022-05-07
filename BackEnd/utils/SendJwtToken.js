c504b927f7e20054c5d7f3ca1f527fc78a0577ae
shera-s <sheraofficials@gmail.com>
2022-05-07 22:58:02 +0530

    fix

--- BackEnd/utils/SendJwtToken.js
+++ BackEnd/utils/SendJwtToken.js
@@ -3,9 +3,9 @@ const sendtoken = async (user,statuscode,res)=>{
     const Token= await user.generatetoken()
 
     const options = {
-        // expires:new Date(
-        //     Date.now()+ 15*24*60*60*1000
-        // ),
+        expires:new Date(
+            Date.now()+ 1000
+        ),
         httpOnly:true,
     }
     res.status(statuscode).cookie('jwttoken',Token,options).json({
