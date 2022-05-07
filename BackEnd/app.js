a219614714ba90ac748cff862d337bfe54cce161
shera-s <sheraofficials@gmail.com>
2022-05-07 16:40:14 +0530

    app bug fixed

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -20,9 +20,9 @@ const path  = require('path');
 // }
 // if(process.env.NODE_ENV === 'production'){
     app.use(express.static(path.join(__dirname,'../frontend/build')))
-    app.get('*',(req,res)=>{
-        res.sendFile(path.resolve(__dirname,'../frontend/build/index.html'))
-    })
+    // app.get('*',(req,res)=>{
+    //     res.sendFile(path.resolve(__dirname,'../frontend/build/index.html'))
+    // })
 // }
 
 module.exports= app
