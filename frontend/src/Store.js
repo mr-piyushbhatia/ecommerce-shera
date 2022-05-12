91cc4080b37fede4ae66b037525efbb8754070a5
shera-s <sheraofficials@gmail.com>
2022-05-12 11:16:53 +0530

    commit

--- frontend/src/Store.js
+++ frontend/src/Store.js
@@ -1,6 +1,6 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
-import { composeWithDevTools } from "redux-devtools-extension/developmentOnly";
+// import { composeWithDevTools } from "redux-devtools-extension/developmentOnly";
 import {
   allproductReviewsReducer,
   deletereviewReducer,
@@ -59,8 +59,7 @@ const middleware = [thunk];
 
 const store = createStore(
   reducer,
-  initialState,
-  composeWithDevTools(applyMiddleware(...middleware))
+  initialState,applyMiddleware(...middleware)
 );
 
 export default store;
