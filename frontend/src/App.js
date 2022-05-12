4fb2b734bdfae5375be597593790ef7ac818a96e
shera-s <sheraofficials@gmail.com>
2022-05-12 15:44:42 +0530

    commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,152 +1,151 @@
 import React, { useEffect } from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-// import { loaduseraction } from "./Actions/useraction";
-// import { useDispatch, useSelector } from "react-redux";
-// import loadable from 'react-loadable'
-import Temp from "./Temp";
+import { loaduseraction } from "./Actions/useraction";
+import { useDispatch, useSelector } from "react-redux";
+import loadable from 'react-loadable'
 
-// const ProtectedRoute = loadable({
-//   loader: ()=> import("./ProtectedRoute"),
-//   loading: ()=> <div></div>
-// })
-// const Header = loadable({
-//   loader: ()=> import("./Components/Layout/Header/Header"),
-//   loading: ()=> <div></div>
-// })
-// const Footer = loadable({
-//   loader: ()=> import("./Components/Layout/Footer/Footer"),
-//   loading: ()=> <div></div>
-// })
-// const UserOptions = loadable({
-//   loader: ()=> import("./Components/Layout/Header/UserOptions"),
-//   loading: ()=> <div></div>
-// })
-// const Home = loadable({
-//   loader: ()=> import("./Components/Home/Home"),
-//   loading: ()=> <div></div>
-// })
-// const ProductDetails = loadable({
-//   loader: ()=> import("./Components/Product/ProductDetails"),
-//   loading: ()=> <div></div>
-// })
-// const Products = loadable({
-//   loader: ()=> import("./Components/Product/Products"),
-//   loading: ()=> <div></div>
-// })
-// const Login_Register = loadable({
-//   loader: ()=> import("./Components/User/Login_Register"),
-//   loading: ()=> <div></div>
-// })
-// const Error404 = loadable({
-//   loader: ()=> import("./Components/Layout/404/Error404"),
-//   loading: ()=> <div></div>
-// })
-// const Cart = loadable({
-//   loader: ()=> import("./Components/Cart/Cart"),
-//   loading: ()=> <div></div>
-// })
-// const MyOrders = loadable({
-//   loader: ()=> import("./Components/Order/MyOrders"),
-//   loading: ()=> <div></div>
-// })
-// const OrderDetails = loadable({
-//   loader: ()=> import("./Components/Order/MyOrders"),
-//   loading: ()=> <div></div>
-// })
-// const Shipping = loadable({
-//   loader: ()=> import("./Components/Cart/Shipping"),
-//   loading: ()=> <div></div>
-// })
-// const ConfirmOrder = loadable({
-//   loader: ()=> import("./Components/Cart/ConfirmOrder"),
-//   loading: ()=> <div></div>
-// })
-// const Payment = loadable({
-//   loader: ()=> import("./Components/Cart/Payment"),
-//   loading: ()=> <div></div>
-// })
-// const Dashboard = loadable({
-//   loader: ()=> import("./Components/Admin/Dashboard"),
-//   loading: ()=> <div></div>
-// })
-// const ProductList = loadable({
-//   loader: ()=> import("./Components/Admin/ProductList"),
-//   loading: ()=> <div></div>
-// })
-// const OrderList = loadable({
-//   loader: ()=> import("./Components/Admin/OrderList"),
-//   loading: ()=> <div></div>
-// })
-// const UsersList = loadable({
-//   loader: ()=> import("./Components/Admin/UsersList"),
-//   loading: ()=> <div></div>
-// })
-// const UpdateUser = loadable({
-//   loader: ()=> import("./Components/Admin/UpdateUser"),
-//   loading: ()=> <div></div>
-// })
-// const NewProduct = loadable({
-//   loader: ()=> import("./Components/Admin/NewProduct"),
-//   loading: ()=> <div></div>
-// })
-// const UpdateProduct = loadable({
-//   loader: ()=> import("./Components/Admin/UpdateProduct"),
-//   loading: ()=> <div></div>
-// })
-// const ProductReviews = loadable({
-//   loader: ()=> import("./Components/Admin/ProductReviews"),
-//   loading: ()=> <div></div>
-// })
-// const Profile = loadable({
-//   loader: ()=> import("./Components/User/Profile"),
-//   loading: ()=> <div></div>
-// })
-// const UpdateProfile = loadable({
-//   loader: ()=> import("./Components/User/UpdateProfile"),
-//   loading: ()=> <div></div>
-// })
-// const UpdatePassword = loadable({
-//   loader: ()=> import("./Components/User/UpdatePassword"),
-//   loading: ()=> <div></div>
-// })
-// const About = loadable({
-//   loader: ()=> import("./Components/Layout/About/About"),
-//   loading: ()=> <div></div>
-// })
-// const Contact = loadable({
-//   loader: ()=> import("./Components/Layout/Contact/Contact"),
-//   loading: ()=> <div></div>
-// })
-// const ForgotPassword = loadable({
-//   loader: ()=> import("./Components/User/ForgotPassword"),
-//   loading: ()=> <div></div>
-// })
-// const ResetPassword = loadable({
-//   loader: ()=> import("./Components/User/ResetPassword"),
-//   loading: ()=> <div></div>
-// })
+const ProtectedRoute = loadable({
+  loader: ()=> import("./ProtectedRoute"),
+  loading: ()=> <div></div>
+})
+const Header = loadable({
+  loader: ()=> import("./Components/Layout/Header/Header"),
+  loading: ()=> <div></div>
+})
+const Footer = loadable({
+  loader: ()=> import("./Components/Layout/Footer/Footer"),
+  loading: ()=> <div></div>
+})
+const UserOptions = loadable({
+  loader: ()=> import("./Components/Layout/Header/UserOptions"),
+  loading: ()=> <div></div>
+})
+const Home = loadable({
+  loader: ()=> import("./Components/Home/Home"),
+  loading: ()=> <div></div>
+})
+const ProductDetails = loadable({
+  loader: ()=> import("./Components/Product/ProductDetails"),
+  loading: ()=> <div></div>
+})
+const Products = loadable({
+  loader: ()=> import("./Components/Product/Products"),
+  loading: ()=> <div></div>
+})
+const Login_Register = loadable({
+  loader: ()=> import("./Components/User/Login_Register"),
+  loading: ()=> <div></div>
+})
+const Error404 = loadable({
+  loader: ()=> import("./Components/Layout/404/Error404"),
+  loading: ()=> <div></div>
+})
+const Cart = loadable({
+  loader: ()=> import("./Components/Cart/Cart"),
+  loading: ()=> <div></div>
+})
+const MyOrders = loadable({
+  loader: ()=> import("./Components/Order/MyOrders"),
+  loading: ()=> <div></div>
+})
+const OrderDetails = loadable({
+  loader: ()=> import("./Components/Order/MyOrders"),
+  loading: ()=> <div></div>
+})
+const Shipping = loadable({
+  loader: ()=> import("./Components/Cart/Shipping"),
+  loading: ()=> <div></div>
+})
+const ConfirmOrder = loadable({
+  loader: ()=> import("./Components/Cart/ConfirmOrder"),
+  loading: ()=> <div></div>
+})
+const Payment = loadable({
+  loader: ()=> import("./Components/Cart/Payment"),
+  loading: ()=> <div></div>
+})
+const Dashboard = loadable({
+  loader: ()=> import("./Components/Admin/Dashboard"),
+  loading: ()=> <div></div>
+})
+const ProductList = loadable({
+  loader: ()=> import("./Components/Admin/ProductList"),
+  loading: ()=> <div></div>
+})
+const OrderList = loadable({
+  loader: ()=> import("./Components/Admin/OrderList"),
+  loading: ()=> <div></div>
+})
+const UsersList = loadable({
+  loader: ()=> import("./Components/Admin/UsersList"),
+  loading: ()=> <div></div>
+})
+const UpdateUser = loadable({
+  loader: ()=> import("./Components/Admin/UpdateUser"),
+  loading: ()=> <div></div>
+})
+const NewProduct = loadable({
+  loader: ()=> import("./Components/Admin/NewProduct"),
+  loading: ()=> <div></div>
+})
+const UpdateProduct = loadable({
+  loader: ()=> import("./Components/Admin/UpdateProduct"),
+  loading: ()=> <div></div>
+})
+const ProductReviews = loadable({
+  loader: ()=> import("./Components/Admin/ProductReviews"),
+  loading: ()=> <div></div>
+})
+const Profile = loadable({
+  loader: ()=> import("./Components/User/Profile"),
+  loading: ()=> <div></div>
+})
+const UpdateProfile = loadable({
+  loader: ()=> import("./Components/User/UpdateProfile"),
+  loading: ()=> <div></div>
+})
+const UpdatePassword = loadable({
+  loader: ()=> import("./Components/User/UpdatePassword"),
+  loading: ()=> <div></div>
+})
+const About = loadable({
+  loader: ()=> import("./Components/Layout/About/About"),
+  loading: ()=> <div></div>
+})
+const Contact = loadable({
+  loader: ()=> import("./Components/Layout/Contact/Contact"),
+  loading: ()=> <div></div>
+})
+const ForgotPassword = loadable({
+  loader: ()=> import("./Components/User/ForgotPassword"),
+  loading: ()=> <div></div>
+})
+const ResetPassword = loadable({
+  loader: ()=> import("./Components/User/ResetPassword"),
+  loading: ()=> <div></div>
+})
 
 
 
 const App = () => {
-  // const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-  // // const { isAuthenticated, user } = useSelector((state) => state.user);
+  const { isAuthenticated, user } = useSelector((state) => state.user);
 
-  // // useEffect(() => {
-  // //   dispatch(loaduseraction());
-  // // }, [dispatch]);
+  useEffect(() => {
+    dispatch(loaduseraction());
+  }, [dispatch]);
 
   return (
     <>
-      {/* <Header /> */}
+      <Header />
       <Router>
-      {/* {isAuthenticated && <UserOptions user={user} />} */}
-        {/* <Switch> */}
+      {isAuthenticated && <UserOptions user={user} />}
+        <Switch>
 
-          <Route exact path="/" component={Temp} />
-          {/* <Route exact path="/about" component={About} />
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={About} />
           <Route exact path="/contact" component={Contact} />
 
           <Route exact path="/product/:id" component={ProductDetails} />
@@ -183,9 +182,9 @@ const App = () => {
           <ProtectedRoute isAdmin={true} exact path='/admin/reviews' component={ProductReviews} />
 
           <Route exact component={Error404} />
-        </Switch> */}
+        </Switch>
       </Router>
-      {/* <Footer /> */}
+      <Footer />
     </>
   );
 };
--- frontend/src/Store.js
+++ frontend/src/Store.js
@@ -1,6 +1,6 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
-// import { composeWithDevTools } from "redux-devtools-extension/developmentOnly";
+import { composeWithDevTools } from "redux-devtools-extension/developmentOnly";
 import {
   allproductReviewsReducer,
   deletereviewReducer,
@@ -59,7 +59,8 @@ const middleware = [thunk];
 
 const store = createStore(
   reducer,
-  initialState,applyMiddleware(...middleware)
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
 );
 
 export default store;
