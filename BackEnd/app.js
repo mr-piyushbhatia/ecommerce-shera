27adc6d06d5ff86d6e97748cda9b58edc004fb86
shera-s <sheraofficials@gmail.com>
2022-05-17 22:04:50 +0530

    commit

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -26,7 +26,6 @@ app.use("/api/v1", emailotp);
 if(process.env.NODE_ENV === 'production'){
     app.get('*.js', function(req, res, next) {
         req.url = req.url + '.gz';
-        console.log(req.url)
         res.set('Content-Encoding', 'gzip');
         res.set('Content-Type', 'text/javascript');
         next();
